8a6d91f534f975b97c56b15b567ab2d355035d21
Paweł <121982293+Nealz1@users.noreply.github.com>
2024-06-18 20:28:05 +0200

    Update and rename Lab 3 zad 2.cpp to Sito Atkina-Bernsteina.cpp

--- Sito Atkina-Bernsteina.cpp
+++ Sito Atkina-Bernsteina.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-//                  Algorytmy i Struktury Danych
-// Zadanie 3.2
-//  Sito Atkina-Bernsteina
-//
-//  WCY21IX4S0  Miksiewicz Pawel
-//============================================================================
-
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
